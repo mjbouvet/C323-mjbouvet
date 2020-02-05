d1595afc3b70bc73ff9e11499da7a90cd0dd1b2d
mjbouvet <mjbouvet@iu.edu>
2020-02-04 19:08:27 -0500

    Lab07

---
+++ labs/lab07/Guessing Game/Tabbed Guessing Game/GameViewController.swift
@@ -0,0 +1,71 @@
+//
+//  GameViewController.swift
+//  Tabbed Guessing Game
+//
+//  Created by Bouvette Jr, Michael John on 1/28/20.
+//  Copyright © 2020 C323 / Spring 2020. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    var input = " "
+    var numberToBeGuessed = Int( arc4random_uniform(10))
+    var answer = Int(arc4random_uniform(20))
+    var answerinput = " "
+    var history : [String] = []
+    var stringtosave = ""
+    var count = 0
+    
+    @IBOutlet var answernumber: UILabel!
+
+    
+    @IBAction func genHigherRandom(_ sender: Any){
+        numberToBeGuessed +=  Int(arc4random_uniform(UInt32(20 - numberToBeGuessed)))
+        input = String(numberToBeGuessed)
+        self.numberLabel.text = input
+        
+           count += 1
+        
+        if(numberToBeGuessed == answer){
+            answerinput = String(numberToBeGuessed)
+            self.answerLabel.text = "It took \(count) tries and the answer is \(answerinput)"
+        }
+        
+        answerinput = String(answer)
+        self.answernumber.text = answerinput
+        
+     
+        
+    }
+    
+    @IBAction func genLowerRandom(_ sender: Any){
+        numberToBeGuessed -= Int( arc4random_uniform(UInt32(numberToBeGuessed)))
+        input = String(numberToBeGuessed)
+        self.numberLabel.text = input
+        
+        count += 1
+        
+        if(numberToBeGuessed == answer)
+        {
+            answerinput = String(numberToBeGuessed)
+            stringtosave = "It took \(count) tries and the answer is \(answerinput)"
+            self.answerLabel.text = stringtosave
+            history.append(stringtosave)
+        }
+        
+        answerinput = String(answer)
+        self.answernumber.text = answerinput
+        
+        
+    }
+}
+
---
+++ labs/lab07/Guessing Game/Tabbed Guessing Game/TabbedGuessingGameModel.swift
@@ -0,0 +1,19 @@
+//
+//  TabbedGuessingGameModel.swift
+//  Tabbed Guessing Game
+//
+//  Created by Bouvette Jr, Michael John on 2/4/20.
+//  Copyright © 2020 C323 / Spring 2020. All rights reserved.
+//
+
+import Foundation
+
+
+class tabGameModel{
+    var history : [String] = []
+
+    init(){
+        
+    }
+    
+}
